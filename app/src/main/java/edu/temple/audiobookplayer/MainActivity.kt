85ed683b1858e9b0089ee26caa6c2c7422a49393
Syn555 <89527340+Syn555@users.noreply.github.com>
2022-03-27 02:23:16 -0400

    Finished. And fixed bug on landscape

---
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -0,0 +1,38 @@
+package edu.temple.audiobookplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity(), BookListFragment.BookListFragmentInterface {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val title = resources.getStringArray(R.array.title)
+        val author = resources.getStringArray(R.array.author)
+        val bookList = BookList()
+        for(i in 0..9){
+            bookList.add(BookObject(title[i],author[i]))
+        }
+        val fragment = supportFragmentManager.findFragmentById(R.id.container1)
+        if(fragment != null)
+            supportFragmentManager.beginTransaction().remove(fragment).commit()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container1, BookListFragment.newInstance(bookList))
+            .commit()
+
+    }
+
+    override fun bookSelected() {
+        if(findViewById<View>(R.id.container2) == null)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container1,BookDetailsFragment())
+                .addToBackStack(null)
+                .commit()
+    }
+
+
+}
